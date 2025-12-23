5fcda561f8ef53887f5155816a2c7df80591f2f6
Igor Stevanovic <igor.d.stevanovic@outlook.com>
2025-12-23 22:53:59 +0100

    chore: remove debug and unused Allure setup files

--- allure-test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Minimal test to verify Allure output
- */
-
-describe('Allure Test', () => {
-  test('should write to allure-results', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
--- jest.allure-only.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'allure-jest/jsdom',
-  testEnvironmentOptions: {
-    resultsDir: 'allure-results'
-  },
-  testMatch: ['**/minimal.test.js']
-};
